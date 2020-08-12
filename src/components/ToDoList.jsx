6fb79f205f00571f46b470155860d13ec337c5ea
Andrey Androsov <irondrondron@gmail.com>
2020-08-12 14:16:24 +0300

    create react app

---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+export default class ToDoList extends React.Component {
+  render() {
+    return (
+      
+        <Button variant="contained" color="secondary">
+          ksdjfndsfnj
+        </Button>
+    );
+  }
+}
---
+++ src/components/UserContext.js
@@ -0,0 +1,9 @@
+import { createContext } from 'react';
+
+export const UserContext = createContext(null);
+
+// const theme = createMuiTheme({
+//   palette: {
+//     type: darkMode ? 'dark' : 'light',
+//   },
+// });
