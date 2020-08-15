d2cd060f8676dfed73238faa705ea72f90142db3
Andrey Androsov <irondrondron@gmail.com>
2020-08-16 02:31:35 +0300

    some styles to default theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,10 @@ import React, { useState } from 'react';
 import { Paper } from '@material-ui/core';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/';
 import Header from './components/Header';
-import TodoItems from './components/TodoList';
-import Todo from './components/Todo';
-import TodoTabs from './components/TodoTabs';
-import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
+import { blue, red } from '@material-ui/core/colors';
 
-export default function App({ children }) {
+export default function App() {
   const [darkMode, setDarkMode] = useState(true);
   const changeTheme = () => {
     setDarkMode(!darkMode);
@@ -16,23 +13,10 @@ export default function App({ children }) {
   const theme = createMuiTheme({
     palette: {
       type: darkMode ? 'dark' : 'light',
+      primary: { main: blue[600] },
+      secondary: { main: red[400] },
     },
   });
-  // const updateTodoToShow = (s) => {
-  //   this.setState({
-  //     todoToShow: s,
-  //   });
-  // };
-
-
-  //   if (state.todoToShow === 'all') {
-  //     todos = state.todos;
-  //   } else if (state.todoToShow === 'active') {
-  //     todos = state.todos.filter((todo) => !todo.complete);
-  //   } else if (state.todoToShow === 'complete') {
-  //     todos = state.todos.filter((todo) => todo.complete);
-  //   }
-  
 
   return (
     <ThemeProvider theme={theme}>
@@ -41,39 +25,7 @@ export default function App({ children }) {
         style={{ width: '100%', minHeight: '100vh', height: 'auto' }}
       >
         <Header setDarkMode={changeTheme} darkMode={darkMode} />
-        {/* <TodoForm onSubmit={addTodo} /> */}
-        {/* {children} */}
-        {/* <h6>{JSON.stringify (state.todos)}</h6> */}
-
-        {/* <TodoForm onSubmit={addTodo} /> */}
-
-        {/* {todos.map((todo) => (
-          <Todo
-            key={todo.id}
-            toggleComplete={toggleComplete(todo.id)}
-            // onDelete={() => this.handleDeleteTodo(todo.id)}
-            todo={todo}
-          />
-        ))} */}
-
-        <TodoList
-        />
-
-        {/* <TodoItems
-          // qq={state.todos.id}
-          toTodo={state.todos.map((todo) => (
-            <Todo
-              key={todo.id}
-            />
-          ))}
-        /> */}
-
-        {/* <TodoTabs
-          allTodosLength={state.todos.filter((todo) => todo).length}
-          activeLength={state.todos.filter((todo) => !todo.complete).length}
-          completeLength={state.todos.filter((todo) => todo.complete).length}
-          complete={state.todos.filter((todo) => todo.complete)}
-        /> */}
+        <TodoList />
       </Paper>
     </ThemeProvider>
   );
